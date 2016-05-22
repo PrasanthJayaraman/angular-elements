e38a65cf1a5c040daeecfbba0d33e2b46fc0441c
PrasanthJayaraman <prasanthj@outlook.com>
2016-05-22 15:11:46 +0530

    code commented

--- angularwidgets/angular-widgets.js
+++ angularwidgets/angular-widgets.js
@@ -1,8 +1,9 @@
 
 
   //Angular Widgets Wrapper
-  angular.module('angularWidgets', ['formDirective', 'matchDirective', 'minMaxDirective', 'checkboxDirective']);
+  angular.module('angularWidgets', ['formDirective', 'matchDirective', 'minMaxDirective', 'widgetDirective']);
 
+  //form directive to create form
   angular.module('formDirective', [])
   .directive('formFor', ['$templateCache','$http', function ($templateCache, $http) {
     return {
@@ -12,10 +13,12 @@
 
             var formNameCounter = 1;
 
+            // if no form name assigned then assign a name automatically
             randomFormName = function() {
                 return "myForm" + (formNameCounter++);
             }
 
+            // check whether the <widget> tag is a child of form (parent)
           var isDirectChild = function(form, el) {
                 var testel;
                 testel = el;
@@ -32,6 +35,7 @@
                 return false;
             };
 
+            // create jqlite elements for each attribute and set the attributes to incoming input element
             __hasProp = {}.hasOwnProperty;
             var setAttrs = function(el, attrs) {
                 var attr, value, _results;
@@ -44,7 +48,7 @@
                 return _results;
             };
 
-
+            // Common for input controls like text, date, email, number, password, url
             var inputControl = '<div class="control-group"><label class="control-label" for="::id">::label <span class="error">::asterisk</span></label>\
                                 <div class="controls" ng-class="{errField: (::formName.::name.$dirty && ::formName.::name.$invalid && ::formName.::name.$error.required)}">::content</div>\
                                 <span class="error" ng-if="::reqSpan" ng-class="{show: (::formName.::name.$error.required && ::formName.::name.$dirty && ::formName.::name.$invalid)}" style="display: none">::label is required</span>\
@@ -55,12 +59,15 @@
                                 <span class="error" ng-if="::miValSpan" ng-class="{show: ::formName.::name.$error.ngMin}" style="display: none">::label should be minimum ::minvalue</span>\
                                 <span class="error" ng-if="::maValSpan" ng-class="{show: ::formName.::name.$error.ngMax}" style="display:none">::label should be maximum ::maxvalue</span>\
                                 </div>';
+
+            // common for textarea, button, submit
             var emptyControl = '<div class="control-group">::content</div>';
             var model = el.attr('model');
             var formName, input, inputGenerator, _ref, _i, _len, _ref, tplEle, actualControl;
             formName = attr.name || randomFormName();
             attr.$set('name', formName);
             attr.$set('novalidate');
+            //get the attributes of the widget tag and generate native angular attributes
             inputGenerator = function (el) {
                 var attrs, _i, _len, _ref, inputEl, spanEl, spanArray = [], asterisk, reqSpan = false, lessLenSpan = false, moreLenSpan = false, miValSpan = false, maValSpan = false, matchSpan = false;
                 var input = angular.element(el);
@@ -105,7 +112,9 @@
                     maValSpan = true
                 }
                 if(typeof match != 'undefined') {
-                    matchSpan = true
+                    matchSpan = true;
+                    var matched = match;
+                    attributes['match'] = model + '.' + matched;
                 }
                 attributes['ng-model'] = modelName;
                 attributes['name'] = name;
@@ -162,36 +171,56 @@
             tplEle = el.clone();
             var root = tplEle[0];
             _ref = tplEle.find("widget");
+            // count the number of 'widget elements' inside form and call inputGenerator and replace the element with native element
             for (_i = 0, _len = _ref.length; _i < _len; _i++) {
                 input = _ref[_i];
                 if (isDirectChild(root, input)) {
                     angular.element(input).replaceWith(inputGenerator(input));
                 }
             }
+            //render the overall template
             template = tplEle.html();
             return template;
         }
     };
   }]);
 
+  // match directive to match two fields
   angular.module('matchDirective', [])
     .directive('match', [function () {
           return {
               require: 'ngModel',
-              link: function (scope, elem, attrs, ctrl) {
-                  var firstField = '#' + attrs.match;
-                  $(elem).add(firstField).on('keyup', function () {
-                      scope.$apply(function () {
-                          var v = elem.val()===$(firstField).val();
-                          ctrl.$setValidity('matchError', v);
-                      });
+              scope: {
+                  match: '='
+              },
+              link: function(scope, element, attrs, ctrl) {
+                  scope.$watch(function() {
+                      var combined;
+
+                      if (scope.match || ctrl.$viewValue) {
+                         combined = scope.match + '_' + ctrl.$viewValue;
+                      }
+                      return combined;
+                  }, function(value) {
+                      if (value) {
+                          ctrl.$parsers.unshift(function(viewValue) {
+                              var origin = scope.match;
+                              if (origin !== viewValue) {
+                                  ctrl.$setValidity("matchError", false);
+                                  return undefined;
+                              } else {
+                                  ctrl.$setValidity("matchError", true);
+                                  return viewValue;
+                              }
+                          });
+                      }
                   });
               }
           };
     }]);
 
 
-
+  // to check min max value required
   angular.module('minMaxDirective', [])
       .directive('minValue', function () {
           return {
@@ -201,10 +230,11 @@
                   function isEmpty(value) {
                       return angular.isUndefined(value) || value === '' || value === null || value !== value;
                   }
-
+                  //watch the value
                   scope.$watch(attr.minValue, function () {
                       ctrl.$setViewValue(ctrl.$viewValue);
                   });
+                  // check the model value with value assigned to min-value attribute
                   var minValidator = function (value) {
                       var min = scope.$eval(attr.minValue) || 0;
                       if (!isEmpty(value) && value < min) {
@@ -234,6 +264,7 @@
                   scope.$watch(attr.maxValue, function () {
                       ctrl.$setViewValue(ctrl.$viewValue);
                   });
+                  // check the model value with value assigned to max-value attribute
                   var maxValidator = function (value) {
                       var max = scope.$eval(attr.maxValue) || Infinity;
                       if (!isEmpty(value) && value > max) {
@@ -251,8 +282,10 @@
           };
   });
 
-  angular.module('checkboxDirective', [])
+  // to generate custom widgets like select, radio, checkbox
+  angular.module('widgetDirective', [])
       .directive('widgetDrop', function() {
+          // get the data and render the dropdown using ng-options
           return {
               restrict: 'EA',
               scope: {
@@ -263,6 +296,7 @@
               },
               template: '<label>{{title}}</label><select ng-model="model" class="form-control"  ng-change="selectedItemsChanged({selectedItem:model})" ng-options="item.value as item.name for item in items track by item.value"><option value="" selected>--select an option--</option></select>',
               link: function (scope, elem, attr) {
+                  // get the selected value and pass it to the scope variable
                   var selected = scope.model || '';
                   scope.selectedItemsChanged = function (item) {
                       scope.model = item.selectedItem;
@@ -271,6 +305,7 @@
           }
       })
 
+      // this generates checkbox
       .directive('widgetCheck', function() {
           return {
               restrict: 'EA',
@@ -281,6 +316,7 @@
               },
               template: '<label>{{title}}</label><div ng-repeat="item in items"><label><input type="checkbox" ng-model="model" value="item.value" ng-change="checkboxChanged({item:item})"  />{{item.name}}</label></div>',
               link: function(scope, element, attrs) {
+                  // get the selected item and check if already available in array if not push else pull
                   var selected = scope.model || [];
                   scope.checkboxChanged = function(obj) {
                       var idx = selected.indexOf(obj.item);
@@ -295,7 +331,9 @@
           }
       })
 
+      // to generate radio buttons
       .directive('widgetRadio', function() {
+          //generated using ng-repeat
           return {
               restrict: 'EA',
               scope: {
@@ -306,6 +344,7 @@
               },
               template: '<label>{{title}}</label><div ng-repeat="item in items"><label><input type="radio" ng-model="model" name="name" ng-value="item.value" ng-click="radioChanged({item:item})"  />{{item.name}}</label></div>',
               link: function(scope, element, attrs) {
+                  //pass th value to the model
                   scope.model = scope.model || '';
                   scope.radioChanged = function(obj) {
                       scope.model = '';
